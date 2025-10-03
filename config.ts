86a9e26d989b2dfc58feff9a89716664c0a27b2f
WZT006 <Jordaynesy@gmail.com>
2025-10-03 08:59:10 +0800

    git ignore fixes

--- config.ts
+++
@@ -1,8 +0,0 @@
-// config.ts
-export const CREDENTIALS = {
-  email: 'winzentt606@gmail.com',
-  password: 'Presoak6.Viselike.Roman',
-};
-export const CONFIG = {
-  URL : 'https://app.anycase.ai/'
-}
